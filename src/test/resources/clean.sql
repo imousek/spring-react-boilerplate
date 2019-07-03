bd9743ecd41043d83d2d30ffe3a2e30b9fdd286d
robo <imousek@gmail.com>
2019-07-03 16:26:40 +0200

    konecne

--- src/test/resources/clean.sql
+++
@@ -1,8 +0,0 @@
-delete from book;
-
-delete from user_role;
-delete from role;
-delete from user;
-
-
-
--- src/test/resources/insert.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO book (id, name, price) VALUES (1, 'Spring Boot React Example', 0.0);
